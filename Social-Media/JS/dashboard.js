69e6eb69734d43571a3f4255516667baaa85d1cf
SumitKumar8252 <sk497623@gmail.com>
2025-07-19 16:05:16 +0530

    commit

--- Social-Media/JS/dashboard.js
+++ Social-Media/JS/dashboard.js
@@ -5,7 +5,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 
-//Menue dropdown button
+//Menu dropdown button
 const profileIcon = document.getElementById("profileIcon");
 const dropdownMenu = document.getElementById("dropdownMenu");
 
@@ -14,8 +14,24 @@ profileIcon.addEventListener("click", () => {
     dropdownMenu.style.display === "block" ? "none" : "block";
 });
 
-window.addEventListener("click", function (e) {
-  if (!document.getElementById("profileMenu").contains(e.target)) {
+window.addEventListener("click", function (event) {
+  if (!document.getElementById("profileMenu").contains(event.target)) {
     dropdownMenu.style.display = "none";
   }
 });
+
+
+//aside container
+const all= document.getElementById('all')
+const instagram= document.getElementById('instagram')
+const thread= document.getElementById('thread')
+const linkedin= document.getElementById('linkedin')
+const facebook= document.getElementById('facebook')
+const twitter= document.getElementById('twitter')
+const pintrest= document.getElementById('pintrest')
+const quora= document.getElementById('quora')
+const youtube= document.getElementById('youtube')
+
+const sideInsta= instagram.addEventListener('click', async()=>{
+  
+})
